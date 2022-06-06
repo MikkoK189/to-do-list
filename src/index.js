9ec8dcc34be7277f04013f90cc6e5d3f9fb9c02c
MikkoK189 <mikkok11@gmail.com>
2022-06-06 20:48:53 +0300

    Starting JS.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import "./style.css";
 import createBase from "./createBaseHTML";
 
 createBase();
-a;
-a;
+
+const newButton = document.getElementById("new-button");
+newButton.addEventListener("click", function () {
+  console.log("TEST");
+});
+const projectsButton = document.getElementById("projects-button");
+projectsButton.addEventListener("click", function () {
+  console.log("PROJECTS");
+});
