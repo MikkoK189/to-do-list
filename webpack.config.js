bc8cbd3e8b707084757d14107052318006b28fe3
MikkoK189 <mikkok11@gmail.com>
2022-06-13 16:46:02 +0300

    Webpack plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const RemoveConsolePlugin = require("remove-console-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -12,6 +13,7 @@ module.exports = {
       filename: "index.html",
       template: "./src/index.html",
     }),
+    new RemoveConsolePlugin(),
   ],
   module: {
     rules: [
